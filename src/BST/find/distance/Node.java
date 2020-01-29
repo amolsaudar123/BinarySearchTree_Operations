0e95b4522e5c003eb2333cd7fa0cc156de75333f
Amol <amol.saudar@guest-tek.com>
2020-01-29 16:52:50 +0530

    - Test Cases has been completed

--- src/BST/find/distance/Node.java
+++ src/BST/find/distance/Node.java
@@ -1,9 +1,9 @@
 package BST.find.distance;
 
 public class Node {
-    int data;
-    Node left;
-    Node right;
+   public int data;
+   public Node left;
+    public Node right;
 
     public Node(int data) {
         this.data = data;
